76fe1b3679edf30438571d301bf4db315856db09
Yusuf-Kenan <kenan2404@gmail.com>
2023-05-30 18:11:34 +0300

    handleSubmit with React.FormEvent<HTMLFormElement>

--- src/pages/todo-page/index.tsx
+++ src/pages/todo-page/index.tsx
@@ -1,59 +1,60 @@
 import { useState } from "react";
-import {
-  Button,
-  Col,
-  Modal,
-  Row,
-  Table,
-  Form,
-  InputGroup,
-} from "react-bootstrap";
+import { Button, Col, Modal, Row, Table, Form } from "react-bootstrap";
 
 type TodoType = { id: number; title: string; is_done: boolean };
 
 export default function TodoPage() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState<boolean>(false);
   const [todos, setTodos] = useState<TodoType[]>([
     { id: 1, title: "test", is_done: false },
     { id: 2, title: "test 2", is_done: true },
     { id: 3, title: "test 3", is_done: false },
   ]);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const value = Object.fromEntries(data.entries());
+    todos.push({
+      id: todos.length+1,
+      title: value.title as string,
+      is_done: value.is_done ? true : false,
+    });
+
+    setTodos([...todos]);
+    handleClose()
+  };
 
   return (
     <>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>To Do</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group className="mb-3">
               <Form.Label>Title</Form.Label>
               <Form.Control
-                type="email"
-                placeholder="type your to do"
-                autoFocus
+                name="title"
+                type="text"
+                placeholder="type your to do..."
               />
             </Form.Group>
-            <InputGroup className="mb-3 d-flex align-items-center">
-            
-              <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-              <Form.Label className="ms-3 mt-3">Is Done </Form.Label>
-            </InputGroup>
-            <InputGroup />
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save
-          </Button>
-        </Modal.Footer>
+            <Form.Group className="mb-3">
+              <Form.Check name="is_done" type="checkbox" label="Done?" />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="button" variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
 
       <div className="pricing-header p-3 pb-md-4 mx-auto text-center">
@@ -75,12 +76,19 @@ export default function TodoPage() {
               </tr>
             </thead>
             <tbody>
-              {todos.map((item: TodoType, index: number) => {
+              {todos.map<JSX.Element>((item: TodoType, index: number) => {
                 return (
                   <tr key={index}>
                     <td>{item.id}</td>
                     <td>{item.title}</td>
-                    <td>{item.is_done ? "Done" : "Undone"}</td>
+                    <td>
+                      <input
+                        className="me-3"
+                        type="checkbox"
+                        checked={item.is_done}
+                      />
+                      {item.is_done ? "Done" : "Undone"}
+                    </td>
                     <td>
                       <Button variant="danger" className="btn btn-sm me-3">
                         Del
